72dc70f34ce0f88defd8f2585dea86431d964b40
pantsidou-dotcom <228503864+pantsidou-dotcom@users.noreply.github.com>
2025-08-28 16:15:43 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ padding: 24, fontFamily: "system-ui, sans-serif" }}>
       <h1>MuseumBuddy – het werkt!</h1>
-      <p>Build zonder TypeScript, lekker simpel.</p>
+      <p>Build zonder TypeScript.</p>
     </main>
   );
 }
