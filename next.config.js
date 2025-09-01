ca1e9dad58c8481c6454270fed19064a6cc10091
pantsidou-dotcom <228503864+pantsidou-dotcom@users.noreply.github.com>
2025-09-01 17:27:33 +0200

    fix: disable next image optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export'
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
-export default nextConfig;
+
+module.exports = nextConfig;
