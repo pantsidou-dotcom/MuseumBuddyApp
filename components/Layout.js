7b96b070d775bc905e011d7961744f12a3b09fca
pantsidou-dotcom <228503864+pantsidou-dotcom@users.noreply.github.com>
2025-09-03 12:41:22 +0200

    Remove museum.nl link

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,6 @@ export default function Layout({ children }) {
           <div className="navspacer" />
           {/* Eventuele navigatie-items voor later */}
           <Link href="/" className="navlink">Musea</Link>
-          <a href="https://museum.nl" target="_blank" rel="noreferrer" className="navlink">museum.nl</a>
         </nav>
       </header>
       <main className="container">{children}</main>
