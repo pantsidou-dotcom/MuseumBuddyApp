e20865e7696ab0b4e706199367b59139ebee57ec
pantsidou-dotcom <228503864+pantsidou-dotcom@users.noreply.github.com>
2025-09-04 15:20:31 +0200

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true
-  // Belangrijk: GEEN "output: 'export'"
-};
-
-module.exports = nextConfig;
