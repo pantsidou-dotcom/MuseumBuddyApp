a030a07826437e6586ba8b6e8d9e7b04130699f7
pantsidou-dotcom <228503864+pantsidou-dotcom@users.noreply.github.com>
2025-09-04 15:08:58 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 import Head from 'next/head';
 import { useMemo, useState } from 'react';
 import Link from 'next/link';
-import { supabase } from '@/lib/supabase';
+import { supabase } from '../lib/supabase';
 import MuseumCard from '../components/MuseumCard';
 
 // We halen data server-side op, zodat je live DB gebruikt
