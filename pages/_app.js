e00855076700c80be0f7287de1f3f123719b140a
pantsidou-dotcom <228503864+pantsidou-dotcom@users.noreply.github.com>
2025-09-06 17:13:13 +0200

    Optimize header logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,9 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
-import { Quicksand } from 'next/font/google';
-
-// Load Quicksand and expose it as a CSS variable so it can be used globally
-const quicksand = Quicksand({
-  subsets: ['latin'],
-  variable: '--font-quicksand',
-  weight: ['400', '500', '700'],
-  display: 'swap',
-});
-
 export default function MyApp({ Component, pageProps }) {
   return (
-    <div className={quicksand.variable}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </div>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
