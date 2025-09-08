649a8cd0c7731b9bd75b0d21ec6c130f28bcdbf2
pantsidou-dotcom <228503864+pantsidou-dotcom@users.noreply.github.com>
2025-09-08 13:10:55 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,16 @@
 import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Header from '../components/Header';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
+}
+import '../styles/globals.css';
 import Layout from '../components/Layout';
 export default function MyApp({ Component, pageProps }) {
   return (
