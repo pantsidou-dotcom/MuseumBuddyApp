5f7d6333d9a6e2f84dbe4d2a1b69f14fc3f95146
pantsidou-dotcom <228503864+pantsidou-dotcom@users.noreply.github.com>
2025-09-09 14:56:05 +0200

    Add about page and navigation link

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function AboutPage() {
+  return (
+    <>
+      <Head>
+        <title>About MuseumBuddy</title>
+      </Head>
+      <h1 className="page-title">About MuseumBuddy</h1>
+      <p>This page will contain information about MuseumBuddy.</p>
+    </>
+  );
+}
