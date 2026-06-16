12c11ac0f977414f95d40eb05bcf326eef314102
pantsidou-dotcom <228503864+pantsidou-dotcom@users.noreply.github.com>
2026-06-16 11:39:24 +0200

    Add SEO landing pages for museum filters

--- pages/geschiedenis-musea-amsterdam.js
+++ pages/geschiedenis-musea-amsterdam.js
@@ -1,6 +1,6 @@
 import { FilterLandingPage, LANDING_PAGE_CONFIGS } from '../lib/seoLandingPages';
 
-const CANONICAL_PATH = '/moderne-kunst-musea-amsterdam';
+const CANONICAL_PATH = '/geschiedenis-musea-amsterdam';
 
 export default function LandingPage() {
   return <FilterLandingPage config={LANDING_PAGE_CONFIGS[CANONICAL_PATH]} canonicalPath={CANONICAL_PATH} />;
